3b1397cb83608cb84252ea8e04eb3c0e4c3d30cf
yqlbu <yqlbu@Bu.edu>
2021-05-02 23:14:59 +0800

    added opencv cloud functions

---
+++ functions/webserver-fn-opencv/func.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+
+	fdk "github.com/fnproject/fdk-go"
+)
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+	// read file stream
+	file, err := os.Open("./static/opencv/opencv-4.1.1-install.sh")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert file stream to bytes
+	data, e := io.ReadAll(file)
+	if e != nil {
+		log.Fatal(e)
+	}
+	// os.Stdout.Write(data)
+
+	log.Print("Inside Go webserver function")
+
+	// write byte output
+	out.Write(data)
+}
