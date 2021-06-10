9446dde40502deaf85170c3e48840b0d1b33980d
yqlbu <yqlbu@Bu.edu>
2021-06-10 16:43:27 +0800

    added api-gateway invoke

--- examples/aws-lambda/main.go
+++ examples/aws-lambda/main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"fmt"
-
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Request struct {
-	Data string `json:"data"`
-}
-
-type Response struct {
-	StatusCode    string `json:"statusCode"`
-	StatusMessage string `json:"statusMessage"`
-	Body          Body
-}
-
 type Body struct {
 	Message string `json:"message"`
+	Data    string `json:"data"`
 }
 
-func HandleRequest(ctx context.Context, request Request) (Response, error) {
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
 
-	fmt.Sprintf("data: %s", request.Data)
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
 
-	return Response{
-		StatusCode:    "200",
-		StatusMessage: "Success",
-		Body:          Body{Message: "received"},
+	// process data
+	rawIn, err := json.Marshal(Body{Message: "Request received!", Data: request.Body})
+	if err != nil {
+		panic(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		IsBase64Encoded: false,
+		StatusCode:      200,
+		Body:            string(rawIn),
 	}, nil
 }
 
--- examples/aws-lambda/main.tf
+++ examples/aws-lambda/main.tf
@@ -1,9 +1,9 @@
-variable region {
+variable "region" {
   type    = string
   default = "ap-northeast-1"
 }
 
-variable image_tag {
+variable "image_tag" {
   type        = string
   description = "The version tag to the container image"
   default     = "latest"
@@ -15,7 +15,6 @@ provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
 }
 
-
 locals {
   prefix              = "dev"
   account_id          = "<account_id>"
@@ -24,12 +23,11 @@ locals {
   ecr_image_tag       = var.image_tag
 }
 
-resource aws_ecr_repository repo {
+### ECR
+
+resource "aws_ecr_repository" "repo" {
   name                     = local.ecr_repository_name
-  image_tag_mutability     = "IMMUTABLE"
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
+  image_tag_mutability     = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -38,10 +36,10 @@ resource aws_ecr_repository repo {
   }
 }
 
-resource null_resource ecr_image {
+resource "null_resource" "ecr_image" {
   triggers = {
     docker_file = md5(file("./Dockerfile"))
-    image_tag = var.image_tag
+    image_tag   = var.image_tag
   }
 
   provisioner "local-exec" {
@@ -54,7 +52,7 @@ resource null_resource ecr_image {
  }
 }
 
-data aws_ecr_image lambda_image {
+data "aws_ecr_image" "lambda_image" {
   depends_on = [
     null_resource.ecr_image
   ]
@@ -62,9 +60,75 @@ data aws_ecr_image lambda_image {
   image_tag       = local.ecr_image_tag
 }
 
-resource "aws_lambda_function" "dev-go-lambda" {
-  function_name = "dev-go-lambda"
-  role          = "arn:aws:iam::${local.account_id}:role/${local.role}"
+### IAM
+
+resource "aws_iam_role" "lambda" {
+  name = "${local.prefix}-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow"
+       }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect = "Allow"
+    resources = [ "*" ]
+    sid = "CreateCloudWatchLogs"
+  }
+
+  statement {
+    actions = [
+      "apigateway:*"
+    ]
+    effect = "Allow"
+    resources = [ "arn:aws:apigateway:*::/*" ]
+    sid = "APIGatewayControl"
+  }
+
+  statement {
+    actions = [
+      "execute-api:Invoke",
+      "execute-api:ManageConnections"
+    ]
+    effect = "Allow"
+    resources = [ "arn:aws:execute-api:*:*:*" ]
+    sid = "InvokeAPI"
+  }
+}
+
+resource "aws_iam_policy" "lambda" {
+  name = "${local.prefix}-lambda-policy"
+  path = "/"
+  policy = data.aws_iam_policy_document.lambda.json
+}
+
+resource "aws_iam_policy_attachment" "lambda" {
+  name       = "${local.prefix}-lambda-policy"
+  roles      = [aws_iam_role.lambda.name]
+  policy_arn = aws_iam_policy.lambda.arn
+}
+
+### Lambda Function
+
+resource "aws_lambda_function" "dev_go_lambda" {
+  function_name = "dev_go_lambda"
+  role          = aws_iam_role.lambda.arn
   image_uri     = "${aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
   package_type  = "Image"
   memory_size   = 128
@@ -74,6 +138,57 @@ resource "aws_lambda_function" "dev-go-lambda" {
   }
 }
 
+### API Gateway
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "api_dev_go_lambda"
+  description = "default lambda function gateway"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.method.http_method
+
+  integration_http_method = "ANY"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.dev_go_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.integration,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "api_invoke_lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.dev_go_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*"
+}
+
+
+### Outputs
+
 output "lambda_name" {
-  value = aws_lambda_function.dev-go-lambda.id
+  value = aws_lambda_function.dev_go_lambda.id
+}
+
+output "version" {
+  value = var.image_tag
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
